ea1eb45ebc51b6517e1d54f80b8cb30b8774a6f6
Abhi Bhatt <bobkinder993@gmail.com>
2022-11-03 09:38:28 -0500

    Commit

--- Frequency.java
+++ Frequency.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.text.NumberFormat;
 import java.util.Random;
 import java.util.Arrays;
+import java.util.Collections;
 
 
 
@@ -38,13 +39,18 @@ public class Frequency {
         
         
         Random rand = new Random();
+        String input1 = "";
         int input2 = 0;
         int input = 0;
         int n = 0;
+        int store = 0;
         int test = 0;
         int random = 0;
         int limit = 51;
+        int[] frequencyIndex = new int[limit];
+        int[] list2 = new int[limit];
         int[] list = new int[limit];
+        int[] sort = new int[limit];
         // ***** objects *****
 
         Scanner scanner = new Scanner(System.in);
@@ -93,12 +99,12 @@ public class Frequency {
          System.out.println("List" + Arrays.toString(list));
         // read input from keyboard
         
-        System.out.println("Press 1 to see frequency of one number, press 2 for a range of numbers? 1/2");
+        System.out.println("Press 1 to see frequency of one number, press 2 for a range of numbers?");
         input = scanner.nextInt();
         if (input == 1) {
             System.out.println("What numbers frequency do you want to see?");
             input = scanner.nextInt();
-            System.out.println(list[input]);
+            System.out.println("The Frequency of " + input + " is " + list[input]);
         }
         else if (input == 2){
             System.out.println("What range of frequency do you want to see?");
@@ -106,14 +112,74 @@ public class Frequency {
             input2 = scanner.nextInt();
             
             for (int i = input -1; i < input2; i++) {
-                System.out.println(list[i]);
+                System.out.println("The Frequency of " + (i + 1) + " " + list[i]);
             }
     }
     else {
         System.out.println("Error");
     }
         // echo input back to console window
+        
+        for (int i = 0; i < limit; i++) {
+            list2[i] = list[i];
+         }
 
+    System.out.println("\n----------------------------");
+    System.out.println("Further frequency challenge");
+    System.out.println("Press 1 to see a number that appears n times, Press 2 to see most/least frequant");
+    input = scanner.nextInt();
+    if (input == 1) {
+        System.out.println("What frequency do you want to see that appears n times");
+        input = scanner.nextInt();
+        for (int i  = 0; i < limit; i++) {
+            if (input == list[i]) {
+                System.out.println(i + " has a frequency of " + input);
+            }
+        }
+    } else if(input == 2) {
+            System.out.println("First type in the range of frequent numbers you want to see. Ex. 5 10");
+            System.out.println("Followed by 'most' or 'least' frequent");
+
+            input = scanner.nextInt();
+            input2 = scanner.nextInt();
+            input1 = scanner.nextLine();
+            for (int j = 0; j < limit; j++) {
+                for (int k = 0; k < limit; k++) {
+                    if (list2[k] > store) {
+                        store = list2[k];
+                        test = k;
+                       // System.out.println(store + " " +  list2[k] + " " + test);
+                    }
+                    //System.out.println(list2[k]);
+                }
+                sort[j] = store;
+                list2[test] = 0;
+                frequencyIndex[j] = test;
+                store = 0;
+            }
+            System.out.println("List sort" + Arrays.toString(sort));
+            if (input1.contains("most")) {
+                System.out.println("The most frequent input in the range of " + input + " to " + input2 + " contains these most frequent numbers: ");
+                for (int i = input -1; i < input2; i++) {
+                    System.out.println("The Frequency of " + frequencyIndex[i] + " showed up " + sort[i]);
+                }
+            }
+            else if (input1.contains("least")) {
+                System.out.println("The least frequent input in the range of " + input + " to " + input2 + " contains these most frequent numbers: ");
+                
+                for (int i = input2 -1; i >= input; i--) {
+                    System.out.println("The Frequency of " + frequencyIndex[i] + " showed up " + sort[i]);
+                }
+            }
+            else {
+                System.out.println("Error");
+            }
+            }
+        
+     else {
+        System.out.println("Error");
+    
+    }
         // ***** Main Processing *****
 
         // ***** Print Formatted Output *****
